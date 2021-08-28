1b03979f81a6b5f0066c6afc65d66d5f5c912890
toyatch <57097594+toyatch@users.noreply.github.com>
2021-08-29 01:11:07 +0900

    modify swiper

--- conf/swiper.el
+++ conf/swiper.el
@@ -2,6 +2,6 @@
 
 ; keybinds
 ;; 検索中: ("M-q") 'swiper-query-replace
-(global-set-key (kbd "C-s")     'swiper)
-(global-set-key (kbd "M-s")     'swiper-all-thing-at-point)
+(global-set-key (kbd "C-s")     'swiper-all-thing-at-point)
+(global-set-key (kbd "M-s")     'swiper)
 (global-set-key (kbd "C-M-s")   'swiper-all-thing-at-point)
