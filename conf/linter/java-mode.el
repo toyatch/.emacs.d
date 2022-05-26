98ec187e5ee6df35beda76cdd883124347e624cb
toyatch <57097594+toyatch@users.noreply.github.com>
2022-05-26 20:00:05 +0900

    lsp-mode for java mode

--- conf/linter/java-mode.el
+++ conf/linter/java-mode.el
@@ -1,3 +1,5 @@
+(unless (require 'lsp-java nil t) (package-refresh-contents) (package-install 'lsp-java) (require 'llsp-java))
+
 (add-hook 'java-mode-hook (lambda ()
                             (setq indent-tabs-mode nil)
                             ))
