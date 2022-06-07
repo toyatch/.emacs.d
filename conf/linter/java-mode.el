8e7a9894756c319dd6d3b65508e74b3cdf29bba3
toyatch <57097594+toyatch@users.noreply.github.com>
2022-06-07 22:32:58 +0900

    java mode

--- conf/linter/java-mode.el
+++ conf/linter/java-mode.el
@@ -1,5 +1,8 @@
 (unless (require 'lsp-java nil t) (package-refresh-contents) (package-install 'lsp-java) (require 'llsp-java))
 
 (add-hook 'java-mode-hook (lambda ()
-                            (setq indent-tabs-mode nil)
+                            ;(setq indent-tabs-mode nil)
+                            (setq tab-width 4)
+                            (set-face-foreground 'whitespace-tab "gray33")
+                            (set-face-background 'whitespace-tab nil)
                             ))
