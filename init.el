3b9908175fba907b6bdd080aa2cb2baa653f51ab
toyatch <57097594+toyatch@users.noreply.github.com>
2023-09-11 22:23:36 +0900

    straight.elでエラーになるので無効化

--- init.el
+++ init.el
@@ -16,20 +16,6 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;; straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
 ;; use-packageの初期化
 (eval-when-compile
   (require 'use-package))
